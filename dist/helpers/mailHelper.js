92c0e082457aa05f74fdfbb713268d42c83e5ae3
nam08102003 <ducnamle81003@gmail.com>
2023-03-08 02:37:12 +0700

    update

--- dist/helpers/mailHelper.js
+++ dist/helpers/mailHelper.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,50 +17,53 @@ const nodemailer_1 = __importDefault(require("nodemailer"));
 const config_1 = require("../config/config");
 const mailTemplate_1 = require("./mailTemplate");
 const sender = nodemailer_1.default.createTransport({
-    host: 'smtp.sendgrid.net',
+    host: 'smtp.gmail.com',
     port: 465,
+    secure: true,
     service: config_1.emailConfig.emailService,
     auth: {
+        type: 'OAUTH2',
         user: config_1.emailConfig.emailUser,
         pass: config_1.emailConfig.emailPassword
-    },
-    secure: true
+    }
 });
-const sendOTP = (email, otp, titleMail, template) => {
-    return (0, exports.sendMail)({
+const sendOTP = (email, otp, titleMail, template) => __awaiter(void 0, void 0, void 0, function* () {
+    return yield (0, exports.sendMail)({
         to: email,
         html: template({ otp }),
         subject: titleMail
     });
-};
+});
 exports.sendOTP = sendOTP;
 exports.sendMail = (exports.sendMail = function (details) {
-    const mailOptions = {
-        to: details.to,
-        subject: details.subject,
-        html: details.html,
-        attachments: details.attachments || [],
-        cc: details.cc || null,
-        bcc: details.bcc || null,
-        from: details.from || config_1.emailConfig.emailFrom
-    };
-    return new Promise(function (resolve, reject) {
-        sender.sendMail(mailOptions, function (err) {
-            if (err) {
-                reject(err);
-            }
-            else {
-                resolve(true);
-            }
+    return __awaiter(this, void 0, void 0, function* () {
+        const mailOptions = {
+            to: details.to,
+            subject: details.subject,
+            html: details.html,
+            attachments: details.attachments || [],
+            cc: details.cc || null,
+            bcc: details.bcc || null,
+            from: details.from || config_1.emailConfig.emailFrom
+        };
+        return yield new Promise(function (resolve, reject) {
+            sender.sendMail(mailOptions, function (err) {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve(true);
+                }
+            });
         });
     });
 });
-exports.sendMessage = (email, messageBody, attachment = []) => {
-    return (0, exports.sendMail)({
+exports.sendMessage = (email, messageBody, attachment = []) => __awaiter(void 0, void 0, void 0, function* () {
+    return yield (0, exports.sendMail)({
         to: email,
         attachments: attachment,
         subject: messageBody.subject,
         html: (0, mailTemplate_1.mailTemplate)(messageBody.subject, messageBody.body)
     });
-};
+});
 //# sourceMappingURL=mailHelper.js.map
--- src/helpers/mailHelper.ts
+++ src/helpers/mailHelper.ts
@@ -3,24 +3,30 @@ import { emailConfig } from '../config/config';
 import { mailTemplate } from './mailTemplate';
 
 const sender = nodemailer.createTransport({
-  host: 'smtp.sendgrid.net',
+  host: 'smtp.gmail.com',
   port: 465,
+  secure: true,
   service: emailConfig.emailService,
   auth: {
+    type: 'OAUTH2',
     user: emailConfig.emailUser,
     pass: emailConfig.emailPassword
-  },
-  secure: true
+  }
 });
 
-export const sendOTP = (email: string, otp?: string, titleMail?: string, template?: Function) => {
-  return sendMail({
+export const sendOTP = async (
+  email: string,
+  otp?: string,
+  titleMail?: string,
+  template?: Function
+) => {
+  return await sendMail({
     to: email,
     html: template({ otp }),
     subject: titleMail
   });
 };
-export const sendMail = (exports.sendMail = function (details: {
+export const sendMail = (exports.sendMail = async function (details: {
   to: string;
   subject: string;
   html: string;
@@ -39,7 +45,7 @@ export const sendMail = (exports.sendMail = function (details: {
     from: details.from || emailConfig.emailFrom
   };
 
-  return new Promise(function (resolve, reject) {
+  return await new Promise(function (resolve, reject) {
     sender.sendMail(mailOptions, function (err) {
       if (err) {
         reject(err);
@@ -50,8 +56,8 @@ export const sendMail = (exports.sendMail = function (details: {
   });
 });
 
-exports.sendMessage = (email: string, messageBody: any, attachment: any[] = []) => {
-  return sendMail({
+exports.sendMessage = async (email: string, messageBody: any, attachment: any[] = []) => {
+  return await sendMail({
     to: email,
     attachments: attachment,
     subject: messageBody.subject,
